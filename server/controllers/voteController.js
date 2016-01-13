212e4eb4aa6c413d4e1046e28cc94ed5f739c485
Eric Geniesse <egeniesse@gmail.com>
2016-01-13 12:04:14 -0800

    Finished the controllers as well as the routing!

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -3,9 +3,13 @@ var db = require('../db/index.js');
 
 module.exports = {
 
-	votePost: function(queryObject) {
-		//queryObject should look like {isPositive: true,
-		//postId: postId, userId: userId}
+	votePost: function(req, res) {
+
+		var queryObject = {
+			isPositive : req.body.isPositive,
+			userId : req.body.userId,
+			postId : req.body.postId
+		};
 		var up = queryObject.isPositive ? 1 : -1;
 		db.Vote.create({
 				where: queryObject,
@@ -15,20 +19,21 @@ module.exports = {
 				db.User.findById(queryObject.userId)
 					.then(function(user) {
 						user.update({
-							reputation: user.reputation + up
-						})
-						.then(function() {
-							db.Post.findById(queryObject.postId)
-								.then(function(post) {
-									post.update({
-										votes: post.votes + up
+								reputation: user.reputation + up
+							})
+							.then(function() {
+								db.Post.findById(queryObject.postId)
+									.then(function(post) {
+										post.update({
+											votes: post.votes + up
+										});
 									});
-								});
-						});
+							});
 					});
 
 			});
 	}
+
 
 
 };
