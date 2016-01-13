15c812a322881d5b1a4cfd573ae2b91372182691
Max Kahn <kahn.max@gmail.com>
2016-01-13 10:50:28 -0800

    Preparing for merge with Eric's serverControllers branch.

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ var dbUser = process.env.DBUSER || 'root';
 var dbPass = process.env.DBPASS;
 // var database = process.env.DATABASE || 'jmuspkeyvjzsvvwp';
 // var dbUser = process.env.DBUSER || 'htmaaabw4pe3k9ja';
+var dbHost = 'localhost';
 // var dbHost = process.env.DBHOST || 'jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 
 var db = new Sequelize(database, dbUser, dbPass, {
