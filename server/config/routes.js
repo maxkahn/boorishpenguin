c9af50b3ef4a7dfebbe32a57a63216ad70ddc267
Max Kahn <kahn.max@gmail.com>
2016-01-13 20:08:20 -0800

    preparing for rebase

--- server/config/routes.js
+++ server/config/routes.js
@@ -34,6 +34,7 @@ module.exports = function(app, express) {
 
   app.get('/api/users', userControllers.allUsers);
   app.get('/api/users/:id', userControllers.oneUser);
+
   app.post('/api/signup', userControllers.newUser);
 
   app.get('/api/courses', courseControllers.allCourses);
