ef431ce9bbe3f968b14edacf4c363887ddde31c0
Max Kahn <kahn.max@gmail.com>
2016-01-13 20:08:20 -0800

    preparing for rebase

--- server/server.js
+++ server/server.js
@@ -40,7 +40,9 @@ var api_url = process.env.API_URL || 'http://127.0.0.1:8001';
 passport.use(new GoogleStrategy({
   clientID: apikeys.googleOauth.clientID,
   clientSecret: apikeys.googleOauth.clientSecret,
+
   callbackURL: api_url + "/auth/google/callback"
+
 
 },
   function(accessToken, refreshToken, profile, done) {
