cc4ac73ad8c00e6eede2c69f3b0c0281fd7a57bc
Ranjit Rao <ranjiterao@gmail.com>
2016-01-14 20:11:46 -0800

    removed api keys

--- server/config/apikeys.js
+++ server/config/apikeys.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleOauth: {
-    clientID: "1005872548855-id5k8k64ol0arnmcaf6s7drpfkcdpp26.apps.googleusercontent.com",
-    clientSecret: "8FEQrTeXLI-kgGoIOUmR8bwS"
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
   }
 }
