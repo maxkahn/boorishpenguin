dd0c464144790eaf9cdd2bc991d475e6d15d0d53
Eric Geniesse <egeniesse@gmail.com>
2016-01-14 19:16:25 -0800

    Finished the necessary methods to add teachers

---
+++ server/controllers/adminControllers.js
@@ -0,0 +1,28 @@
+var db = require('../db/index.js');
+
+module.exports = {
+	toggleTeacherAccess : function(req, res) {
+		db.User.findById(req.body.userId)
+			.then(function(admin){
+				if(admin.isAdmin){
+				db.User.findById(req.params.id)
+					.then(function(user) {
+						user.updateAttributes({
+							isTeacher: !user.isTeacher
+						}).then(function(result) {
+							res.status(200).json(result);
+						});
+					});
+				} else {
+					res.sendStatus(404);
+				}
+			});
+	},
+
+	getPendingTeachers : function(req, res){
+		db.User.findAll({where: { pendingTeacher : true } })
+			.then(function(pendingTeachers){
+				res.status(200).json(pendingTeachers);
+			});
+	}
+};
