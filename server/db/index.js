8f9566c4bda232247eb0203e4753904ca517d9e6
Max Kahn <kahn.max@gmail.com>
2016-01-15 20:53:53 -0800

    config variables and bower as npm dependency

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 
 var database = process.env.DATABASE || 'townhall';
 var dbUser = process.env.DBUSER || 'root';
-var dbHost = process.env.DBHOST || 'localhost';
+var dbHost = process.env.DATABASE_URL || 'localhost';
 var dbPass = process.env.DBPASS;
 
 var db = new Sequelize(database, dbUser, dbPass, {
